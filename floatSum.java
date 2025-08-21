721680a657928c9479045f8197ef31816dec53d5
sajag01 <mishrasajag9@gmail.com>
2025-08-21 13:49:26 +0530

    Add files via upload

---
+++ floatSum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class floatSum {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float a = scn.nextFloat();
+        float b = scn.nextFloat();
+        System.out.print(a+b);
+        scn.close();
+    }
+}
