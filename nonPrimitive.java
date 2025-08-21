7bc1a17b1ed488571bda73f84d9c3e4ba4fb46f7
sajag01 <mishrasajag9@gmail.com>
2025-08-21 13:47:30 +0530

    Add files via upload

---
+++ nonPrimitive.java
@@ -0,0 +1,13 @@
+public class nonPrimitive {
+    public static void main(String[] args) {
+        String name = "Sajag"; 
+        int arr[]= {1,2,3,4};
+        System.out.print(arr[0] + " ");
+        System.out.print(arr[1] + " ");
+        System.out.print(arr[3] + " ");
+        System.out.println(name);
+        int a = 1, b = 2, c = 4, d = 5;
+        float  e = 2.3f, f = 3.2f, g = 9.9f;
+
+    }
+}
